e1efdfb2f22a5225a2a87799905adb2afd89d477
Eva <eva-moore@bk.ru>
2026-09-15 17:06:49 +0300

    Linter errors fixed

--- fit_life.py
+++ fit_life.py
@@ -4,54 +4,68 @@ ML_TO_LITERS = 1000
 
 
 # 1. Знакомство
-# TODO: Спроси у пользователя имя и сохрани в переменную user_name
-# TODO: Спроси возраст и сохрани в переменную user_age (не забудь преобразовать в число)
 print("Приветствую в приложении FitLife!")
 user_name = input("Давайте познакомимся! Как Вас зовут? - ")
 
 try:
-    user_age = int(input(f"Приветствую , {user_name}, теперь введите Ваш возраст: "))
+    user_age = int(
+        input(
+            f"Приветствую, {user_name}, "
+            "теперь введите Ваш возраст: "
+        )
+    )
 except ValueError:
     print("Возраст должен быть целым числом")
 
+
 # 2. Сбор данных
-# TODO: Запроси вес (в кг) и сохрани в user_weight (тип float)
-# TODO: Запроси рост (в метрах, например 1.75) и сохрани в user_height (тип float)
 try:
-    user_weight = float(input("Теперь введите, пожалуйста, Ваш вес в килограммах: "))
+    user_weight = float(
+        input(
+            "Теперь введите, пожалуйста, "
+            "Ваш вес в килограммах: "
+        )
+    )
 except ValueError:
     print("Введите вес числовым значением - пример: 60.5")
 
 try:
-    user_height = float(input("И ваш рост в метрах: "))
+    user_height = float(
+        input("И ваш рост в метрах: ")
+    )
 except ValueError:
-    print("Введите вес числовым значением - пример: 1.65")
+    print("Введите рост числовым значением - пример: 1.65")
 
 
-# 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
-# Формула ИМТ: вес разделить на (рост в квадрате)
-# TODO: Рассчитай bmi (Индекс массы тела)
+# 3. Логика расчетов
 def bmi_calculation(weight: float, height: float):
-    bmi = weight / (height ** 2)
+    """Calculate BMI using weight and height."""
+    return weight / (height ** 2)
 
-    return bmi
 
-# Подсчет воды: вес * 30 мл
-# TODO: Рассчитай water_needed
 def water_calculation(weight: float):
-    water_ml = weight * WATER_PER_KG
-    water_liters = water_ml / ML_TO_LITERS
-    return water_liters
+    """Calculate recommended daily water intake in liters."""
+    return weight * WATER_PER_KG / ML_TO_LITERS
+
 
 # 4. Вывод красивого результата
-# TODO: Используй f-строку, чтобы вывести приветствие, например: "Привет, Иван!"
-# TODO: Выведи возраст, ИМТ (округленный до 1 знака) и норму воды.
 def report():
+    """Print the user's FitLife report."""
     print()
-    print(f"Отчет для пользователя: {user_name} ({user_age} г.)")
-    print(f"Твой Индекс Массы Тела: {round(bmi_calculation(user_weight, user_height), 1)}")
-    print(f"Рекомендуемая норма воды: {round(water_calculation(user_weight), 1)} л. в день")
+    print(
+        f"Отчет для пользователя: "
+        f"{user_name} ({user_age} г.)"
+    )
+    print(
+        f"Твой Индекс Массы Тела: "
+        f"{round(bmi_calculation(user_weight, user_height), 1)}"
+    )
+    print(
+        f"Рекомендуемая норма воды: "
+        f"{round(water_calculation(user_weight), 1)} л. в день"
+    )
     print()
     print("Расчет окончен. Будьте здоровы!")
+
 
 report()
