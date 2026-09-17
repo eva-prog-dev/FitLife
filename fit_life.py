ab1abd8da32eebeaa6520680102be1570efa4356
Eva <eva-moore@bk.ru>
2026-09-17 09:31:15 +0300

    Исправление замечаний

--- fit_life.py
+++ fit_life.py
@@ -4,67 +4,98 @@ ML_TO_LITERS = 1000
 
 # 1. Знакомство c пользователем
 print("Приветствую в приложении FitLife!")
-user_name = input("Давайте познакомимся! Как Вас зовут? - ")
 
-try:
-    user_age = int(
-        input(
-            f"Приветствую, {user_name}, "
-            "теперь введите Ваш возраст: "
+while True:
+    user_name = input(
+        "Давайте познакомимся! Как Вас зовут? - "
+    ).strip()
+    if not user_name:
+        print("Имя не может быть пустым")
+        continue
+
+    if not user_name.isalpha():
+        print("Имя должно содержать только буквы")
+        continue
+
+    break
+
+while True:
+    try:
+        user_age = int(
+            input(
+                f"Приветствую, {user_name}, "
+                "теперь введите Ваш возраст: "
+            )
         )
-    )
-except ValueError:
-    print("Возраст должен быть целым числом")
+    except ValueError:
+        print("Возраст должен быть целым числом")
+        continue
+    if user_age <= 0:
+        print("Возраст должен быть больше 0")
+        continue
+
+    break
 
 
 # 2. Сбор данных
-try:
-    user_weight = float(
-        input(
-            "Теперь введите, пожалуйста, "
-            "Ваш вес в килограммах: "
+while True:
+    try:
+        user_weight = float(
+            input(
+                "Теперь введите, пожалуйста, "
+                "Ваш вес в килограммах: "
+            )
         )
-    )
-except ValueError:
-    print("Введите вес числовым значением - пример: 60.5")
+    except ValueError:
+        print("Введите вес числовым значением - пример: 60.5")
+        continue
+    if user_weight <= 0:
+        print("Вес должен быть больше 0")
+        continue
 
-try:
-    user_height = float(
-        input("И ваш рост в метрах: ")
-    )
-except ValueError:
-    print("Введите рост числовым значением - пример: 1.65")
+    break
+
+while True:
+    try:
+        user_height = float(
+            input("И ваш рост в метрах: ")
+        )
+    except ValueError:
+        print("Введите рост числовым значением - пример: 1.65")
+        continue
+    if user_height <= 0:
+        print("Рост должен быть больше 0")
+        continue
+
+    break
 
 
 # 3. Расчеты ИМТ и нормы воды в день
 def bmi_calculation(weight: float, height: float):
-    """Расчет ИМТ"""
+    """Рассчет индекса массы тела по весу и росту."""
     return weight / (height ** 2)
 
 
 def water_calculation(weight: float):
-    """Расчет необходимого количества воды в день"""
+    """Расчет необходимого количества воды в день."""
     return weight * WATER_PER_KG / ML_TO_LITERS
 
 
 # 4. Вывод результата
 def report():
-    """Печать отчета"""
-    print()
+    """Печать отчета."""
     print(
+        "",
         f"Отчет для пользователя: "
         f"{user_name} ({user_age} г.)",
-    )
-    print(
         f"Твой Индекс Массы Тела: "
         f"{round(bmi_calculation(user_weight, user_height), 1)}",
-    )
-    print(
         f"Рекомендуемая норма воды: "
         f"{round(water_calculation(user_weight), 1)} л. в день",
+        "",
+        "Расчет окончен. Будьте здоровы!",
+        sep="\n",
     )
-    print()
-    print("Расчет окончен. Будьте здоровы!")
 
 
 report()
